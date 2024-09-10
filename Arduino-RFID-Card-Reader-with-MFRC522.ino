c80147de560715f3180c6abdd7999e37d11866ce
Anayet Hossain <125878215+anayet884@users.noreply.github.com>
2024-09-10 08:13:05 +0600

    Add files via upload

---
+++ Arduino-RFID-Card-Reader-with-MFRC522.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+
+void setup() {
+  Serial.begin(9600);   // Initialize serial communications with the PC
+  SPI.begin();          // Initialize SPI bus
+  mfrc522.PCD_Init();   // Initialize MFRC522
+  Serial.println("Place your card near the RFID reader...");
+  Serial.println();
+}
+
+void loop() {
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Show UID on serial monitor
+  Serial.print("Card UID: ");
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+
+  // Halt PICC
+  mfrc522.PICC_HaltA();
+}
